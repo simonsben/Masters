a2810c62d482c4518a5f2c47fd33ae9e904df1df
simonsben <simons.ben0@gmail.com>
2019-05-15 10:31:43 -0400

    Finished writing pre-processing for unicode chracters

--- utilities/pre_processing/special_characters.py
+++ utilities/pre_processing/special_characters.py
@@ -1,14 +1,15 @@
-from re import compile, subn, match
+from re import compile, subn, search
+from unidecode import unidecode
 
 
 special_regex = compile(r'&\S+;')
-special_regex_parser = compile(r'(&amp;)|(&lt;|&le;)|(&gt;|&ge;)|(&160;)|(&201;)')
+special_regex_parser = compile(r'(&amp;)|(&lt;|&le;)|(&gt;|&ge;)|(&160;)|'  # Special characters
+                               r'((?<=&)\d+(?=;))')                         # Accented characters
 group_map = {
     0: 'and',
     1: 'less than',
     2: 'greater than',
-    3: ' ',
-    4: 'e'
+    3: ' '
 }
 
 
@@ -16,13 +17,16 @@ def manage_special_characters(document):
     """ Locates special characters and intelligently replaces (or removes) them """
     def replace(_match):
         character = _match.group(0)
-        parsed = match(special_regex_parser, character)
+        parsed = search(special_regex_parser, character)
         if parsed is not None:
             group_id = get_symbol_index(parsed.groups())
-            return group_map.get(group_id)
+
+            if group_id < 4:
+                return group_map.get(group_id)
+            return get_ascii_char(parsed.group(0))
 
         print(character)
-        return _match.group(0)
+        return ''
 
     document, num_special = subn(special_regex, replace, document)
 
@@ -34,3 +38,11 @@ def get_symbol_index(groups):
         if val is not None:
             return ind
     raise ValueError('Group does not exist')
+
+
+def get_ascii_char(unicode):
+    """ Takes decimal unicode and returns closest ascii character """
+    if type(unicode) == str:
+        unicode = int(unicode)
+
+    return unidecode(chr(unicode))
