773de20ee90a6714cb0d02d1e4d2e3868c59a549
simonsben <simons.ben0@gmail.com>
2019-05-29 09:50:24 -0400

    Finished writing stacked predictor... I think

--- execution/prediction/stacked.py
+++ execution/prediction/stacked.py
@@ -1,5 +1,6 @@
 from utilities.data_management import move_to_root, open_w_pandas, make_path, check_existence, check_writable
-from model.training import train_xg_boost
+from xgboost import XGBClassifier
+from sklearn.preprocessing import LabelEncoder
 
 move_to_root()
 
@@ -17,16 +18,25 @@ check_existence(test_path)
 check_writable(model_path)
 
 # Import data
-train_set = open_w_pandas(train_path).to_numpy()
-test_set = open_w_pandas(test_path).to_numpy()
+train_set = open_w_pandas(train_path)
+test_set = open_w_pandas(test_path)
 labels = open_w_pandas(dataset_path)['is_abusive'].to_numpy().astype(bool)
 train_labels, test_labels = labels[:len(train_set)], labels[len(test_set):]
-print('Data loaded')
+print('Data loaded, making predictions')
 
-# Train predictor
-classifier = train_xg_boost(train_set, train_labels, prepared=True, quiet=False)
-print('Model trained')
+# Load model
+model = XGBClassifier(objective='binary:logistic', n_estimators=600, silent=True)
+model.load_model(str(model_path))
+model._le = LabelEncoder().fit([0, 1])
 
-# Save model
-classifier.save_model(str(model_path))
-print('Model saved')
+# Make predictions
+train_set['stacked'] = model.predict(train_set.to_numpy())
+test_set['stacked'] = model.predict(test_set.to_numpy())
+
+# Save predictions
+train_set.to_csv(train_path)
+test_set.to_csv(test_path)
+print('Predictions saved')
+
+print(train_set.describe())
+print(test_set.describe())
