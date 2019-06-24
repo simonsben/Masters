e7db34eeb4d35454acb53b1d99193b52cae28b4e
b.simons <b.simons@computing.cs.queensu.ca>
2019-06-24 14:02:11 -0400

    Remove additional pre-processor from n-grams, not effective

--- model/extraction/n_grams.py
+++ model/extraction/n_grams.py
@@ -2,10 +2,6 @@ from sklearn.feature_extraction.text import CountVectorizer
 from pandas import DataFrame
 
 
-PREFIX = '<'
-SUFFIX = '>'
-
-
 def n_gram_matrix(dataset, num_features=10000, use_words=True):
     """
     Takes a pre-processed file and constructs a sparse document term matrix.
@@ -24,10 +20,7 @@ def n_gram_matrix(dataset, num_features=10000, use_words=True):
     else:
         vectorizer = CountVectorizer(max_features=num_features, ngram_range=(3, 5), analyzer='char_wb')
 
-    # Define pre-processor
-    pre_processor = lambda doc: ' '.join([PREFIX + word + SUFFIX for word in doc.split(' ') if len(word) > 0])
-
     # Vectorize corpus and convert to a sparse dataframe
-    vector_data = vectorizer.fit_transform(dataset['document_content'].apply(pre_processor))
+    vector_data = vectorizer.fit_transform(dataset['document_content'])
 
     return vector_data, vectorizer.get_feature_names()
