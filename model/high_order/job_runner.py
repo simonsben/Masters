1d1a47d0fbd1e8f015f05cd78ecd866901d7b84b
simonsben <simons.ben0@gmail.com>
2019-09-13 13:07:52 -0400

    Updated job runner to work with intent computation

--- model/high_order/job_runner.py
+++ model/high_order/job_runner.py
@@ -7,6 +7,7 @@ from functools import partial
 
 
 def generate_data_modifier(header_list, data_header):
+    """ Generates a function to select the desired columns from the source data """
     indexes = [data_header[header] for header in header_list]
     max_ind = max(indexes)
 
@@ -19,12 +20,14 @@ def generate_data_modifier(header_list, data_header):
 
 
 def get_path(path):
+    """ Function that ensures that the path given is either a string or Path (and converts to Path) """
     if isinstance(path, (Path, str)):
         return Path(path)
     raise TypeError('Path given must be of type string or Path')
 
 
 def check_processes(processes):
+    """ Checks passed processes to ensure they are callable """
     if callable(processes):
         return [processes]
     elif isinstance(processes, list):
@@ -35,6 +38,7 @@ def check_processes(processes):
 
 
 def processor(document, processes):
+    """ Function that applies multiple process functions to each document """
     for process in processes:
         document = process(document)
     return document
@@ -105,11 +109,12 @@ class job_runner:
         print('Data prepared.')
 
     def finalize_data(self):
+        """ Closes file-streams """
         self.source_file.close()
         self.dest_file.close()
         print('Document finalized.')
 
-    def process_documents(self, data_modifier=None, single_job=True):
+    def process_documents(self, data_modifier=None, single_job=True, max_documents=None):
         """ Processes documents """
         # While there is still data to process
         if not self.data_ready:
@@ -118,36 +123,50 @@ class job_runner:
             self.start_workers()
 
         print('Starting document processing')
-        complete = False
-        chunk_number = 0
+        eof_reached = False
+        documents_processed = chunk_number = 0
+
         while True:
             chunk_number += 1
 
             data_start = time()
-            data = []
-            for index in range(self.chunk_size):
+            data, index = [], 0
+
+            # Enable max_documents functionality
+            limit = self.chunk_size
+            if max_documents is not None and self.chunk_size + documents_processed > max_documents:
+                limit = max_documents
+
+            # Pull documents
+            for index in range(limit):
                 try:
                     document = next(self.csv_reader)
                     data.append(self.data_modifier(document))
                 except StopIteration:
-                    complete = True
+                    eof_reached = True
                     break
 
+            documents_processed += index + 1
             data_time = time() - data_start
 
+            # Process documents
             process_start = time()
             data = self.workers.map(partial(processor, processes=self.processor), data)
             process_time = time() - process_start
 
+            # If it takes more time to load the data then run the process, get larger chunks at a time
             if process_time < data_time:
                 self.chunk_size *= 2
                 print('Process time less than data load time, doubling chunk size to', self.chunk_size)
 
-            self.csv_writer.writerows(enumerate(data))
+            self.csv_writer.writerows(enumerate(data, start=documents_processed - limit))
             print('Finished chunk', chunk_number)
 
-            if complete:
+            # If there are no more documents or there is a max document restriction (that is met)
+            if eof_reached or (max_documents is not None and max_documents <= documents_processed):
                 break
 
-        self.shutdown_workers()
-        self.finalize_data()
+        # If this is the only job to be run, close files and kill workers
+        if single_job:
+            self.shutdown_workers()
+            self.finalize_data()
