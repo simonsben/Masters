40fb01dff51a3ab1e8ed3a1bbc2893227c606f34
b.simons <b.simons@computing.cs.queensu.ca>
2020-01-17 13:51:45 -0500

    Fixed subset extractor + added plotting for base verbs

--- execution/pre_train/pull_subset.py
+++ execution/pre_train/pull_subset.py
@@ -1,5 +1,6 @@
 from utilities.data_management import make_path, move_to_root
 from pandas import read_csv
+from numpy.random import choice
 
 move_to_root()
 
@@ -11,20 +12,22 @@ variants = ['', '_partial']
 num_lines = 250000
 print('Prepared params, starting selection')
 
+subset = None
+
 for variant in variants:
-    source_path, dest_path = base / (source_name + variant + '.csv'), base / (dest_name + variant + '.csv.gz')
+    source_path = base / (source_name + variant + '.csv')
+    dest_path = base / (dest_name + variant + '.csv.gz')
 
     source = read_csv(source_path, index_col=0)
-    sample = source.sample(n=num_lines)
-    sample.to_csv(dest_path)
+    print('Loaded data')
 
-    # dest_fl = dest.open('w')
-    # with source.open('r') as src_fl:
-    #     for index, line in enumerate(src_fl):
-    #         if index > num_lines:
-    #             break
-    #         dest_fl.write(line)
-    # dest_fl.close()
+    # Define the same subset for both variants
+    if subset is None:
+        subset = choice(source.shape[0], size=num_lines, replace=False)
+        print('Defined subset')
+
+    sample = source.iloc[subset]
+    sample.to_csv(dest_path)
 
     print('Finished', variant)
 print('All writes complete.')
