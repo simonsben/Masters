d01250ac93f442923d81d3a64c08e2c4724b56d7
simonsben <simons.ben0@gmail.com>
2020-04-23 16:06:37 -0400

    Added new config template

--- config_template.py
+++ config_template.py
@@ -1,7 +1,20 @@
+from warnings import warn
+
 # Data constants
 dataset = 'dataset_name'
-fast_text_model = 'fast_text_model_name'
+fast_text_model = 'model_name'
 
 # Execution platform constants
 n_threads = 4
-max_tokens = 300
+
+max_token_values = {
+    'dataset_name': 200
+}
+embedding_dimensions = {
+    'model_name': 200
+}
+
+max_tokens = max_token_values[dataset]
+embedding_dimension = embedding_dimensions[fast_text_model]
+
+warn('Loaded execution params with dataset %s and fastText model %s' % (dataset, fast_text_model), RuntimeWarning)
