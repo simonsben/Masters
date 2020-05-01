a88bca1d336ec548e73476dbab4f35218b530b58
simonsben <simons.ben0@gmail.com>
2020-05-01 17:51:04 -0400

    Started re-working networks to enable on-the-fly embedding generation with training and evaluating

--- execution/training/intent_model.py
+++ execution/training/intent_model.py
@@ -1,41 +1,50 @@
-from model.networks import generate_intent_network, generate_convolution_network
+from model.networks import generate_intent_network, generate_convolution_network, generate_attention_network
 from utilities.data_management import make_dir, make_path, open_w_pandas, open_embeddings, check_existence, \
     get_model_path, load_vector, vector_to_file
 from utilities.pre_processing import runtime_clean, token_to_index
 from model.training import train_term_learner, train_deep_learner
 from config import dataset, fast_text_model, max_tokens, mask_refinement_method
 from scipy.sparse import load_npz
-from os import environ
+from fasttext import load_model
+from model.layers.realtime_embedding import RealtimeEmbedding
 
 # Define paths
 intent_weights_path = get_model_path('intent')
-embedding_path = make_path('data/prepared_lexicon/') / ('abusive_intent-' + fast_text_model + '.csv')
+# embedding_path = make_path('data/prepared_lexicon/') / ('abusive_intent-' + fast_text_model + '.csv')
+embedding_path = make_path('data/models/') / dataset / 'derived' / (dataset + '.bin')
 base_path = make_path('data/processed_data/') / dataset / 'analysis'
-context_path = base_path / 'intent' / 'contexts.csv'
-initial_label_path = base_path / 'intent' / (mask_refinement_method + '_mask.csv')
-document_matrix_path = base_path / 'intent' / 'document_matrix.npz'
-label_path = base_path / 'intent' / 'intent_training_labels.csv'
+intent_path = base_path / 'intent'
+context_path = intent_path / 'contexts.csv'
+initial_label_path = intent_path / (mask_refinement_method + '_mask.csv')
+document_matrix_path = intent_path / 'document_matrix.npz'
+label_path = intent_path / 'intent_training_labels.csv'
+token_path = intent_path / 'ngrams.csv'
 
 # Check for files and make directories
-check_existence([embedding_path, context_path, initial_label_path, document_matrix_path])
+check_existence([embedding_path, context_path, initial_label_path, document_matrix_path, token_path])
 make_dir(intent_weights_path.parent)
 print('Config complete.')
 
 # Load embeddings and contexts
-tokens, embeddings = open_embeddings(embedding_path)
+# tokens, embeddings = open_embeddings(embedding_path)
+embedding_model = load_model(str(embedding_path))
+
 raw_contexts = open_w_pandas(context_path)['contexts'].values
 initial_labels = load_vector(initial_label_path)
 document_matrix = load_npz(document_matrix_path)
+tokens = load_vector(token_path)
+token_mapping = {token: index for index, token in enumerate(tokens)}
 print('Loaded data.')
 
 # Clean contexts and enumerate tokens
 contexts = runtime_clean(raw_contexts)
-enumerated_contexts, token_mapping = token_to_index(contexts, tokens, return_mapping=True)
+# enumerated_contexts, token_mapping = token_to_index(contexts, tokens, return_mapping=True)
 print('Prepared data')
 
 # Generate fresh (untrained model)
-model = generate_convolution_network(max_tokens, embedding_matrix=embeddings)
 labels = initial_labels.copy()
+realtime = RealtimeEmbedding(embedding_model, contexts, labels)
+model = generate_intent_network(max_tokens, embedding_dimension=realtime.embedding_dimension)
 print('Generated model\n', model.summary())
 
 rounds = 3  # Number of rounds of training to perform
@@ -45,7 +54,7 @@ for round_num in range(rounds):
     print('Starting full round', round_num + 1, 'of', rounds)
 
     # Train deep model
-    # model, labels, round_prediction = train_deep_learner(model, labels, enumerated_contexts, rounds=1, sub_rounds=)
+    model, labels, round_prediction = train_deep_learner(model, labels, realtime)
 
     # Run term learner
     token_labels = labels.copy()
--- model/training/deep_reinforce.py
+++ model/training/deep_reinforce.py
@@ -1,5 +1,6 @@
 from numpy import all, sum, where, argmax, percentile, abs, min, max, ndarray
 from keras.models import Model
+from model.layers.realtime_embedding import RealtimeEmbedding
 from config import training_verbosity, batch_size
 
 
@@ -19,15 +20,15 @@ def print_bits(values):
     )
 
 
-def train_deep_learner(model, current_labels, enumerated_documents, rounds=3, sub_rounds=3, min_confidence=.985,
+def train_deep_learner(model, current_labels, data_source, rounds=2, training_documents=250000, min_confidence=.985,
                        label_modifier=.4):
     """
-    Performs X rounds of training on deep network to reinforce current labels and generate new set
+    Performs X rounds of training on deep network to learn from then update the current labels
     :param Model model: Keras model to be trained
     :param ndarray current_labels: Current set of document labels
-    :param ndarray enumerated_documents: Array of documents with each token enumerated corresponding to word embeddings
+    :param RealtimeEmbedding data_source: Array of documents with each token enumerated corresponding to word embeddings
     :param int rounds: Number of reinforcement-training rounds [default 3]
-    :param int sub_rounds: Number of epochs in each reinforcement-training round [default 3]
+    :param int training_documents: Number of epochs in each reinforcement-training round [default 3]
     :param float min_confidence: Min predicted value for document to *contain intent* [default .985]
     :param float label_modifier: Modifier applied to current labels [default .4]
     :return model, current labels, new_predictions
@@ -39,38 +40,42 @@ def train_deep_learner(model, current_labels, enumerated_documents, rounds=3, su
     predictions = None
 
     for round_number in range(rounds):
-        training_mask = current_labels != .5    # Only use labels that are not *uncertain*
+        data_source.update_labels(current_labels)
 
         # Get subset of non uncertain data to use for training
-        training_data = enumerated_documents[training_mask]
-        training_labels = current_labels[training_mask]
+        training_mask = current_labels != .5    # Only use labels that are not *uncertain*
+        data_source.set_mask(training_mask)
+
+        # Compute constants for the round
+        steps_per = int(training_documents / batch_size)
 
         # Train model
-        model.fit(training_data, training_labels, batch_size=batch_size, epochs=sub_rounds, verbose=training_verbosity)
+        data_source.set_usage_mode(True)
+        model.fit_generator(data_source, verbose=training_verbosity, steps_per_epoch=steps_per, shuffle=True)
 
         # Make predictions for all documents
-        predictions = model.predict(enumerated_documents, verbose=training_verbosity, batch_size=batch_size).reshape(-1)
+        data_source.set_usage_mode(False)
+        predictions = model.predict_generator(data_source, verbose=training_verbosity)\
+            .reshape(-1)
 
-        # Scale and shift predictions to [0, 1]
-        scaled_predictions = rescale(predictions)
-        print_bits(scaled_predictions)
+        # Limit prediction values to [0, 1]
+        bounded_predictions = predictions.copy()
+        bounded_predictions[bounded_predictions < 0] = 0
+        bounded_predictions[bounded_predictions > 1] = 1
+        print_bits(bounded_predictions)
 
         # Compute mask of documents with positive and negative intent
-        new_positives = scaled_predictions > positive_threshold
-        new_negatives = scaled_predictions < negative_threshold
-
-        # Get indexes of documents with positive and negative intent
-        [pos_indices] = where(new_positives)
-        [neg_indices] = where(new_negatives)
+        new_positives = bounded_predictions > positive_threshold
+        new_negatives = bounded_predictions < negative_threshold
 
         # Apply confidence modifications to new labels
-        current_labels[pos_indices] += label_modifier
-        current_labels[neg_indices] -= label_modifier
+        current_labels[new_positives] += label_modifier
+        current_labels[new_negatives] -= label_modifier
 
         # Restrict label value range to [0, 1]
         current_labels[current_labels < 0] = 0
         current_labels[current_labels > 1] = 1
 
-        print(pos_indices.shape[0] + neg_indices.shape[0], 'classified in deep training round', round_number + 1)
+        print(sum(new_positives) + sum(new_negatives), 'classified in deep training round', round_number + 1)
 
     return model, current_labels, predictions
