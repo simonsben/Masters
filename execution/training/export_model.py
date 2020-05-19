be6810dd5dcc7bdfaeee5f0c00133211f492155c
simonsben <simons.ben0@gmail.com>
2020-05-19 11:23:31 -0400

    Updated model export script

--- execution/training/export_model.py
+++ execution/training/export_model.py
@@ -1,6 +1,5 @@
-from model.networks import generate_intent_network, generate_abuse_network
-from utilities.data_management import get_model_path, make_path, freeze_session
-from tensorflow import io
+from model.networks import generate_abusive_intent_network
+from utilities.data_management import get_model_path, make_path
 import config
 
 # Load config
@@ -11,25 +10,14 @@ embedding_dimension = config.embedding_dimension
 # Define file paths
 intent_weights_path = get_model_path('intent')
 abuse_weights_path = get_model_path('abuse')
+production_path = intent_weights_path.parent / 'production'
 production_dir = make_path('data/models') / dataset / 'analysis'
 
-# Generate intent model and load trained weights
-production_network = generate_intent_network(max_tokens, embedding_dimension=embedding_dimension)
-production_network.load_weights(intent_weights_path, by_name=True)
-print(production_network.summary())
-
-# Convert Keras model to Tensorflow graph and save
-graph = freeze_session(production_network)
-io.write_graph(graph, str(production_dir), 'intent.pb')
-print('Intent model exported.')
-
-
 # Generate abuse model and load trained weights
-production_network = generate_abuse_network(max_tokens, embedding_dimension=embedding_dimension)
+production_network = generate_abusive_intent_network(max_tokens, embedding_dimension=embedding_dimension)
+production_network.load_weights(intent_weights_path, by_name=True)
 production_network.load_weights(abuse_weights_path, by_name=True)
 print(production_network.summary())
 
-# Convert Keras model to Tensorflow graph and save
-graph = freeze_session(production_network)
-io.write_graph(graph, str(production_dir), 'abuse.pb')
-print('Intent model exported.')
+production_network.save(production_path)
+print('Model exported.')
