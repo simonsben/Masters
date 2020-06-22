Fixed sequence-content matrix computation

execution/pre_train/document_term_matrix.py
@@ -1,7 +1,7 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from utilities.data_management import make_path, open_w_pandas, vector_to_file
 from scipy.sparse import save_npz
-from numpy import savetxt
+from numpy import asarray
 import config
 
 # Load execution parameters
@@ -22,9 +22,10 @@ vectorizer = CountVectorizer(ngram_range=(3, 6), max_features=500000, token_patt
 
 # Compute context-term matrix
 document_matrix = vectorizer.fit_transform(contexts)
+sequences = asarray(vectorizer.get_feature_names())
 print('Context ngram matrix computed, saving')
 
 # Save data
 save_npz(matrix_path, document_matrix)
-vector_to_file(vectorizer.get_feature_names(), feature_path)
+vector_to_file(sequences, feature_path)
 print('Save complete')