5199701e8efbcb518565fa548055fa8ba076f663
simonsben <simons.ben0@gmail.com>
2020-07-08 19:56:55 -0400

    Initial re-work of label movement during training analysis

--- execution/analysis/intent/movement_in_training.py
+++ execution/analysis/intent/movement_in_training.py
@@ -1,42 +1,46 @@
 from utilities.data_management import make_path, load_vector, open_w_pandas, check_existence
 # from utilities.plotting import scatter_plot, show
-from numpy import asarray, logical_not, sum, where
-from config import dataset
+from matplotlib.pyplot import subplots, show
+from numpy import asarray, logical_not, sum, where, zeros, arange, abs
+from config import dataset, confidence_increment
+
+num_rounds = 20
 
 base = make_path('data/processed_data/') / dataset / 'analysis'
 data_base = base / 'intent'
 initial_label_path = data_base / 'cone_mask.csv'
-round_label_gen = lambda index: data_base / ('midway_mask_%d_of_10.csv' % index)
+round_label_gen = lambda index: data_base / ('midway_mask_%d_of_%d.csv' % (index, num_rounds))
 context_path = data_base / 'contexts.csv'
-english_mask_path = data_base / 'english_mask.csv'
 
-check_existence([context_path, english_mask_path, initial_label_path])
+check_existence([context_path, initial_label_path])
+print('Config complete.')
 
-# english_mask = load_vector(english_mask_path).astype(bool)
 raw_contexts = open_w_pandas(context_path)
 contexts = raw_contexts['contexts'].values
 
 initial_labels = load_vector(initial_label_path)
-round_labels = asarray([load_vector(round_label_gen(index)) for index in range(10)]).transpose()
+round_labels = asarray([load_vector(round_label_gen(index)) for index in range(num_rounds)])
+print('Loaded data.')
 
-print(contexts.shape, round_labels.shape)
+num_contexts = round_labels.shape[1]
 
+num_possible_labels = int(1 / confidence_increment) + 1
+rounds = arange(0, num_rounds)
+shares = zeros((num_possible_labels, num_rounds), dtype=float)
 
-certain_start = initial_labels != .5
-uncertain_start = logical_not(certain_start)
-positive_end = round_labels[:, -1] >= 1
-negative_end = round_labels[:, -1] <= 0
+label_values = [confidence_increment * index for index in range(num_possible_labels)]
+for index, label_value in enumerate(label_values):
+    shares[index] = sum(abs(round_labels - label_value) < .01, axis=1) / num_contexts
 
-changes = initial_labels != round_labels[:, -1]
+print(rounds.shape, shares.shape)
 
-ground_changes = changes.copy()
-ground_changes[uncertain_start] = False
-[ground_changes] = where(ground_changes)
-print(sum(ground_changes), 'changes to ground-truth labels')
+print(('%5.1f ' * num_possible_labels) % tuple(label_values))
+for round in range(num_rounds):
+    print(('%5.3f ' * num_possible_labels) % tuple(shares[:, round]))
 
-for index in ground_changes:
-    print(round_labels[index], contexts[index])
+fig, ax = subplots(figsize=(10, 7))
+ax.stackplot(rounds, shares, labels=[('%.1f' % value) for value in label_values])
+ax.legend()
+ax.grid()
 
-print(sum(positive_end[uncertain_start]), 'positive documents labelled')
-print(sum(negative_end[uncertain_start]), 'negative documents labelled')
-
+show()
